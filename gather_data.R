163a02dbecea8c3b347013d85dd264c368002153
Sneha Kannan <snehak3@gmail.com>
2020-03-30 22:44:14 -0500

    Resume building code first commit

--- gather_data.R
+++ gather_data.R
@@ -28,9 +28,9 @@ if(using_googlesheets){
   
   # Want to go oldschool with just a csv?
   position_data <- read_csv("csvs/positions.csv")
-  skills        <- read_csv("csvs/language_skills.csv")
-  text_blocks   <- read_csv("csvs/text_blocks.csv")
-  contact_info  <- read_csv("csvs/contact_info.csv", skip = 1)
+  #skills        <- read_csv("csvs/language_skills.csv")
+  #text_blocks   <- read_csv("csvs/text_blocks.csv")
+  #contact_info  <- read_csv("csvs/contact_info.csv", skip = 1)
   
 }
 
